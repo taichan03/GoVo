bcc341c1129d2d2eb845cd28ec878ebafe71f176
taichan03 <tai.c.chan3@gmail.com>
2023-04-14 07:51:02 -0400

    Container

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { Article, Brand, Cta } from './components'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { Article, Brand, Cta } from "./components";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <div className="App">
@@ -30,7 +30,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/containers/whatGPT3/WhatGPT3.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./gpt3.css";
+
+const GPT3 = () => {
+  return <div>GPT3</div>;
+};
+
+export default GPT3;
