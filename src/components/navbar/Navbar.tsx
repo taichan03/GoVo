ff94cb7db4871671710494f33a7594e47b982d1a
taichan03 <tai.c.chan3@gmail.com>
2023-04-18 07:07:28 -0400

    header

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import "./navbar.css";
 import { RiMenu3Line, RiCloseLine } from "react-icons/ri";
 import logo from "../../assets/logo.svg";
-import GovoLogo from "../../assets/GovoLogo.png";
+import govo_logo from "../../assets/govo_logo.png";
 
 const NavBar = () => {
   const [toggleMenu, setToggleMenu] = useState(false);
@@ -10,32 +10,31 @@ const NavBar = () => {
   return (
     <div className="gpt3__navbar">
       <div className="gpt3__navbar-links">
-        <div className="gpt3__navbar-links_logo">
+        {/* <div className="gpt3__navbar-links_logo">
           <img src={logo} alt="logo" />
-        </div>
-        {/* <div className="govo__navbar-links_logo">
-          <img src={GovoLogo} alt="logo" />
         </div> */}
-        <div className="gpt3__navbar-links_container">
-          <p>
-            <a href="#home">Home</a>
-          </p>
-          <p>
-            <a href="#wgpt3">What is GPT?</a>
-          </p>
-          <p>
-            <a href="#possibility">Open AI</a>
-          </p>
-          <p>
-            <a href="#features">Case Studies</a>
-          </p>
-          <p>
-            <a href="#blog">Library</a>
-          </p>
+        <div className="govo__navbar-links_logo">
+          <img src={govo_logo} alt="logo" />
         </div>
       </div>
+      <div className="gpt3__navbar-links_container">
+        <p>
+          <a href="#home">Home</a>
+        </p>
+        <p>
+          <a href="#wgpt3">DEMO</a>
+        </p>
+        <p>
+          <a href="#possibility">Media</a>
+        </p>
+        {/* <p>
+          <a href="#features">Case Studies</a>
+        </p>
+        <p>
+          <a href="#blog">Library</a>
+        </p> */}
+      </div>
       <div className="gpt3__navbar-sign">
-        <p>Sign in</p>
         <button type="button">Sign up</button>
       </div>
       <div className="gpt3__navbar-menu">
--- src/containers/header/Header.tsx
+++ src/containers/header/Header.tsx
@@ -7,28 +7,32 @@ const Header = () => (
   <div className="gpt3__header section__padding" id="home">
     <div className="gpt3__header-content">
       <h1 className="gradient__text">
-        Let&apos;s Build Something amazing with GPT-3 OpenAI
+        A non-partisan app to help you connect the dots
       </h1>
       <p>
-        Yet bed any for travelling assistance indulgence unpleasing. Not
-        thoughts all exercise blessing. Indulgence way everything joy alteration
-        boisterous the attachment. Party we years to order allow asked of.
+        Enter your zip code for a tailored voting plan for the your next
+        upcoming election. Remember, there can be up to 3 elections on any given
+        year and we encourage participation in your local elections for maximum
+        impact.
       </p>
 
       <div className="gpt3__header-content__input">
-        <input type="email" placeholder="Your Email Address" />
+        <input type="email" placeholder="Enter your Zip Code" />
         <button type="button">Get Started</button>
       </div>
 
-      <div className="gpt3__header-content__people">
+      {/* <div className="gpt3__header-content__people">
         <img src={people} />
         <p>1,600 people requested access a visit in last 24 hours</p>
-      </div>
+      </div> */}
     </div>
 
-    <div className="gpt3__header-image">
+    {/* <div className="gpt3__header-image">
       <img src={ai} />
     </div>
+    <div className="gpt3__header-image">
+      <img src={ai} />
+    </div> */}
   </div>
 );
 
