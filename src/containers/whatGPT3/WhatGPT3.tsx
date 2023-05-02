fe5111417d2f67b313342ae2c35ffacaf03a6f08
sixer <tai.c.chan3@gmail.com>
2023-05-01 22:57:44 -0400

    chat

--- src/containers/whatGPT3/WhatGPT3.tsx
+++ src/containers/whatGPT3/WhatGPT3.tsx
@@ -10,6 +10,8 @@ import {
   MessageInput,
   TypingIndicator,
 } from "@chatscope/chat-ui-kit-react";
+import { useEffect } from "react";
+
 
 const API_KEY = "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";
 
@@ -17,19 +19,37 @@ const systemMessage = {
   //  Explain things like you're talking to a software professional with 5 years of experience.
   role: "system",
   content:
-    "Create a voting plan based on the following zip code, and list each candidate party affiliation: ${zipCode}",
+    "Create a voting plan based on the following zip code, and list each candidate party affiliation based on the zipcode.",
 };
 
 function WhatGPT3({ zipCode }) {
   const [messages, setMessages] = useState([
     {
-      message: `Hello, this is GoVo. How can I help you? ${zipCode}`,
+      message: `Hello, this is GoVo.`,
       sentTime: "just now",
       sender: "GoVo",
     },
   ]);
 
   const [isTyping, setIsTyping] = useState(false);
+
+  useEffect(() => {
+    const sendMessageToChatGPT = async () => {
+      const newMessage = {
+        message: `I live in ${zipCode}.`,
+        direction: "outgoing",
+        sender: "user",
+      };
+
+      const newMessages = [...messages, newMessage];
+      setMessages(newMessages);
+
+      setIsTyping(true);
+      await processMessageToChatGPT(newMessages);
+    };
+
+    sendMessageToChatGPT();
+  }, [zipCode]);
 
   const handleSend = async (message) => {
     const newMessage = {
